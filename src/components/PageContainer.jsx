0131c7e3c989c7c67305e6122a36a9cb66ef1eeb
gyuri224 <rbfl0415@gmail.com>
2025-07-07 18:21:04 +0900

    PageContainer 수정

--- src/components/PageContainer.jsx
+++ src/components/PageContainer.jsx
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 
 const PageContainer = styled.div`
   width: 100%;
-  max-width: 375px;   // ✅ Figma 디자인 기준
-  padding: 0 24px;    // ✅ 좌우 여백 유지
-  margin: 0 auto;     // ✅ 가운데 정렬
-  padding-bottom: 137px;
+  max-width: 375px;   
+  padding: 0 24px;    // 좌우 여백 유지
+  margin: 0 auto;     // 가운데 정렬
+  padding-bottom: 147px;
 `;
 
 export default PageContainer;
